0014d250ae15486d79c4abd0ef0ca7acb3678c74
AN.Mamun <96946518+anmamun0@users.noreply.github.com>
2023-09-27 15:15:23 +0600

    2 NO JAVA.java

---
+++ 2 NO JAVA.java
@@ -0,0 +1,24 @@
+public class fast{
+    public static void main(String[] args){
+        int num1 , num2 ;
+        num1 = 270;
+        num2 = 30;
+
+        int result_plus             = num1 + num2;
+        int result_minus            = num1 - num2;
+        int  result_multipication   = num1 * num2;
+
+        int result_divition       = num1 / num2 ;
+        int result_modulus        = num1 % num2 ;
+
+
+        System.out.printf("Your result is + : %d \n", result_plus);
+        System.out.printf("Your result is -: %d \n", result_minus);
+        System.out.printf("Your result is *: %d \n", result_multipication);
+        System.out.printf("Your result is /: %d \n", result_divition);
+        System.out.printf("Your result is %: %d \n", result_modulus);
+
+
+      
+    }
+}
